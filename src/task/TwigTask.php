a70b98c029ac88832bf0a76923c034f307986e84
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-13 12:50:15 +0800

    TwigTask (incomplete)

--- src/task/TwigTask.php
+++ src/task/TwigTask.php
@@ -80,7 +80,9 @@ class TwigTask extends Task
         $template = "{$path['dirname']}/{$path['filename']}";
 
         $this->log(sprintf('Loading %s', $template));
+        /*
         $assetic->setup($template);
         $environment->loadTemplate($template);
+         */
     }
 }
